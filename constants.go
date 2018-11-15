62a3f5fe67dec6a352398636c9e771faebae8b60
Russell Jones <russell.valerius.jones@gmail.com>
2018-11-15 22:36:11 +0000

    Remove Teleport related entries from kubeconfig upon "tsh logout".

--- constants.go
+++ constants.go
@@ -152,6 +152,9 @@ const (
 	// ComponentRBAC is role-based access control.
 	ComponentRBAC = "rbac"
 
+	// ComponentTSH is the "tsh" binary.
+	ComponentTSH = "tsh"
+
 	// DebugEnvVar tells tests to use verbose debug output
 	DebugEnvVar = "DEBUG"
 
